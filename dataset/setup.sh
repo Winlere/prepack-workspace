ef6fdfd949a8db41356a670c754d2e2e1b4a58f8
winlere <winlere@qq.com>
2025-11-08 04:36:15 +0000

    feat: add dataset from Microsft and visualize it

--- dataset/setup.sh
+++ dataset/setup.sh
@@ -4,6 +4,7 @@ set -e
 
 mkdir -p burstgpt
 mkdir -p mooncake
+mkdir -p microsoft
 
 pushd burstgpt
 wget https://github.com/HPMLL/BurstGPT/releases/download/v1.1/BurstGPT_1.csv -O BurstGPT_1.csv &
@@ -13,8 +14,13 @@ wget https://github.com/HPMLL/BurstGPT/releases/download/v1.1/BurstGPT_without_f
 popd
 
 pushd mooncake
-wget https://github.com/kvcache-ai/Mooncake/blob/main/FAST25-release/traces/conversation_trace.jsonl -O conversation_trace.jsonl &
+wget https://raw.githubusercontent.com/kvcache-ai/Mooncake/refs/heads/main/FAST25-release/traces/conversation_trace.jsonl -O conversation_trace.jsonl &
 wget https://raw.githubusercontent.com/kvcache-ai/Mooncake/refs/heads/main/FAST25-release/traces/toolagent_trace.jsonl -O toolagent_trace.jsonl &
 popd
+
+pushd microsoft
+wget https://azurepublicdatasettraces.blob.core.windows.net/azurellminfererencetrace/AzureLLMInferenceTrace_code_1week.csv -O AzureLLMInferenceTrace_code_1week.csv &
+wget https://azurepublicdatasettraces.blob.core.windows.net/azurellminfererencetrace/AzureLLMInferenceTrace_conv_1week.csv -O AzureLLMInferenceTrace_conv_1week.csv &
+popd 
 
 wait
